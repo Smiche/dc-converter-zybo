b846a78a0f5270d2d9c169b4e88cc99548b523be
Forsberi <118350906+Forsberi@users.noreply.github.com>
2022-12-13 12:41:03 +0200

    Update PID.c

--- SwitchingPSU_FreeRTOS/src/PID.c
+++ SwitchingPSU_FreeRTOS/src/PID.c
@@ -2,15 +2,23 @@
 #include <stdio.h>
 #include "PID.h"
 
-float PID(float Kp, float Ki, float Kd, float voltageref, float voltage){
+float PID(float Kp, float Ki, float Kd, float voltageref, float voltage, float saturation_limit){
     static float e, e_prev, yi_prev;
     float yp, yi, yd, out;
+    
     e = voltageref - voltage; // Calculating difference
+    
     // Calculating yp, yi ja yd
     yp = Kp*e;
     yi = yi_prev+Ki*e;
     yd = Kd*e-Kd*e_prev;
+    
+    // Saturation check
+    if(abs(yi)>=saturation_limit)
+        yi=yi_prev;
+    
     out = yp + yi + yd; // PID output
+    
     // Updating old values
     yi_prev = yi;
     e_prev = e;
